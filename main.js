b93e33b0f9faf9abfb785ee67393c145c2f67883
suchi <suchitrarudra04@gmail.com>
2019-07-10 07:50:34 -0700

    Minor changes to html. Adding main process.

---
+++ main.js
@@ -0,0 +1,25 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow; 
+
+// Listen for app to be ready 
+app.on("ready", function() {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+});
+
+// Quit app when closed
+app.on("closed", function() {
+  app.quit();
+});
+
